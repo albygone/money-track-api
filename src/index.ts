b47a538f970320d98a2f1c4b39fd22843c3d1565
Alberto Gonella <albygonella@gmail.com>
2023-06-14 23:33:16 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import * as Realm from 'realm-web';
 import * as utils from './utils';
 
-const dbUri = 'mongodb+srv://albygone:legomania05@maincluster.lri6hlm.mongodb.net/?retryWrites=true&w=majority';
+const dbUri =
+	'mongodb+srv://albygone:legomania05@maincluster.lri6hlm.mongodb.net/?retryWrites=true&w=majority';
 
 // The Worker's environment bindings. See `wrangler.toml` file.
 interface Bindings {
@@ -26,8 +27,6 @@ const ObjectId = Realm.BSON.ObjectId;
 // Define the Worker logic
 const worker: ExportedHandler<Bindings> = {
 	async fetch(req, env) {
-		
-
 		const url = new URL(req.url);
 		App = App || new Realm.App(env.REALM_APPID);
 
@@ -41,16 +40,6 @@ const worker: ExportedHandler<Bindings> = {
 
 		try {
 			if (method === 'GET') {
-				// if (todoID) {
-				// 	// GET /api/todos?id=XXX
-				// 	return utils.reply(
-				// 		await collection.findOne({
-				// 			_id: new ObjectId(todoID),
-				// 		})
-				// 	);
-				// }
-				// GET /api/todos
-				// return utils.reply(await collection.find());
 			}
 
 			// POST /api/todos
@@ -63,7 +52,7 @@ const worker: ExportedHandler<Bindings> = {
 						password: newUserCredentials.password,
 					});
 
-                    utils.reply("coap");
+					utils.reply('coap');
 
 					const credentials = Realm.Credentials.emailPassword(
 						newUserCredentials.email,
@@ -88,18 +77,22 @@ const worker: ExportedHandler<Bindings> = {
 					return utils.reply(user.id);
 				}
 
-				if(path == '/getTransactions'){
+				if (path == '/getTransactions') {
 					let data: any = await req.json();
 					const id = data.id;
-					
-                    const credentials = Realm.Credentials.apiKey("drefwOtN17WPW2h42oCoxuj2Qq5xKmDpsgJxspfnPHMMVQbPntY0BkRLHWsnpq19");
 
-                    var user = await App.logIn(credentials);
-                    var client = user.mongoClient('mongodb-atlas');
+					const credentials = Realm.Credentials.apiKey(
+						'drefwOtN17WPW2h42oCoxuj2Qq5xKmDpsgJxspfnPHMMVQbPntY0BkRLHWsnpq19'
+					);
 
-                    const collection = client.db('moneytrack').collection<Transaction>('transactions');
-                   
-					return utils.reply(await  collection.find({}));
+					var user = await App.logIn(credentials);
+					var client = user.mongoClient('mongodb-atlas');
+
+					const collection = client
+						.db('moneytrack')
+						.collection<Transaction>('transactions');
+
+					return utils.reply(await collection.find({ user_id: id }));
 				}
 			}
 
